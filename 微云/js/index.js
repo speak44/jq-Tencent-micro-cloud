dab3114e8a945aed4afcf3a7cd80f40db39c71c5
speak44 <1246806407@qq.com>
2017-09-28 21:09:23 +0800

    2017-9-28

---
+++ 微云/js/index.js
@@ -0,0 +1,197 @@
+//文件管理
+let task=$('.task');
+//搜索框修改
+let search=$('.search');
+//搜索框
+let ser_text=$('#text');
+//搜索×
+let cuo=search.find('em');
+//添加按钮
+let add_span=$('.addition').find('span');
+//添加项显示内容
+let add_cont=$('.add_cont');
+//列表显示内容
+let js_setli=$('.js_setli');
+//图标显示内容
+let js_setch=$('.js_setch');
+//时间显示内容
+let js_timer=$('.js_timer');
+//a-z显示内容
+let js_azpx=$('.js_azpx');
+//头像信息
+let toux=$('.toux');
+//头像内信息
+let cotl_t1=$('.cotl_t1');
+//文件内容 ul
+let zon_ul=$('.zon_ul');
+//装垃圾箱ul
+let lfmy_ul=$('.lfmy_ul');
+//右侧内容文件
+let zhcoont=$('.zhcoont');
+//面包屑导航栏
+let cotmbx=$('.cotmbx');
+//点击划√的个数
+let n=0;
+//隐藏的头部内容
+let hedhidobj=$('.headhide');
+//全选框
+let checkall=$('.checkall')
+//文件管理点击事件
+task.click(()=>{
+	if (task.data('onoff')) {
+		task.css({'background':'','border':''})
+		task.data('onoff',false)
+	} else{
+		task.css({'background':'url(img/task2.png) no-repeat center','border': '1px solid #3b93ff'})
+		task.data('onoff',true)		
+	}
+	return false
+})
+//搜索框点击事件
+search.click(()=>{
+	$('.js_seri').css({'background':'url(img/search2.png)'})
+	search.animate({
+		width:'292px',
+		borderColor: '#3b93ff'
+	},'show',function(){
+		cuo.show()
+	})
+	 return false;
+})
+//document 点击的时候执行内容
+$(document).click(()=>{
+	$('.js_seri').css({'background':'url(img/search.png)'})
+	cuo.hide()
+	search.animate({
+		width:'200px',
+		borderColor: '#dadbe3'},'show')
+})
+//列表显示内容
+js_setli.click(()=>{
+	js_setli.css({
+		background: '#eeeef2 url(img/setlist.png) no-repeat center' 
+	})
+	js_setch.css({
+		background: 'url(img/setchart.png) no-repeat center' 		
+	})
+})
+//图标显示内容
+js_setch.click(()=>{
+	js_setch.css({
+		background: '#eeeef2 url(img/setchart.png) no-repeat center' 		
+	})
+	js_setli.css({
+		background: 'url(img/setlist.png) no-repeat center' 
+	})	
+})
+//事件显示内容
+js_timer.click(()=>{
+	js_timer.css({
+		background:'#eeeef2 url(img/time.png) no-repeat center'
+	})
+	js_azpx.css({
+		background: 'url(img/a_z.png) no-repeat center'
+	})
+})
+js_azpx.click(()=>{
+	js_azpx.css({
+		background: '#eeeef2 url(img/a_z.png) no-repeat center'
+	})
+	js_timer.css({
+		background:'url(img/time.png) no-repeat center'
+	})	
+})
+//添加项的事件
+//	鼠标移入事件
+add_span.mouseenter(()=>{
+	add_cont.fadeIn('fast')
+})
+//鼠标移出事件
+add_span.mouseleave(()=>{
+	add_cont.fadeOut('fast')
+})
+//
+add_cont.mouseenter(()=>{
+	add_cont.stop()
+})
+add_cont.mouseleave(()=>{
+	add_cont.fadeOut('fast')	
+})
+//头像移入显示信息
+toux.mouseenter(()=>{
+	cotl_t1.fadeIn('fast')
+})
+toux.mouseleave(()=>{
+	cotl_t1.fadeOut('fast')
+})
+cotl_t1.mouseenter(()=>{
+	cotl_t1.stop()
+})
+cotl_t1.mouseleave(()=>{
+	cotl_t1.fadeOut('fast')	
+})
+//左侧ul生成列表 的全部信息一条
+let li=document.createElement('li');
+ $(li).data('path',data[0].path)
+   $(li).addClass('loall')
+$(li).html(
+	`<i style="background:url(${data[0].pic}) no-repeat center"></i><span>${data[0].name}<span>`
+	)
+zon_ul.append(li)
+//渲染左侧列表剩余内容
+list(data)
+//添加垃圾箱的li标签
+let lzli=document.createElement('li');
+$(lzli).data('path',data[1].path)
+// $(lzli).addClass('lename')
+$(lzli).html(
+	`<i style="background:url(${data[1].pic}) no-repeat center"></i><span>${data[1].name}</span>`
+)
+lfmy_ul.append(lzli)
+//渲染右侧内容文件
+contwen(data)
+/*********************************以上内容为渲染页面***************/
+window.onhashchange=(()=>{
+	//hash改变找到父级
+	let Data=getdata(data)
+	//hash改变后再次渲染右侧文件夹内容
+	newcont(Data,data)
+	//面包屑导航生成
+	let arr=getdata2(data)
+	nav(arr)
+	//文件每个的删除按钮
+	wendel()
+	//右侧内容双击事件
+	dbclk()
+	//左侧内容点击事件
+	lefclick()
+	//全部 li的点击事件
+	lefallclick()
+	//面包屑点击事件
+	rinavclick()
+	//文件单一选中的事件
+	wenchecked()
+	//触发全选按钮事件
+	chall()
+	//行间重命名事件
+	rename()
+	
+})
+//右侧内容双击事件
+dbclk()
+//左侧内容点击事件
+lefclick()
+//全部 li的点击事件
+lefallclick()
+//面包屑点击事件
+rinavclick()
+//文件单一选中的事件
+wenchecked()
+//触发全选按钮事件
+chall()
+//文件每个的删除按钮
+wendel()
+//行间重命名事件
+rename()
+//全局调用框选
+boxSelect();
---
+++ 微云/js/tool.js
@@ -0,0 +1,608 @@
+//左侧ul连内容的渲染
+function list(data){
+	let list=null
+	$('.lename').remove()
+	for (let k in data[0].child) {
+		if (k!='maxId') {
+			lis=document.createElement('li')
+			$(lis).data('path',data[0].child[k].path)
+			$(lis).addClass('lename')
+			$(lis).html(`
+				<i style="background:url(${data[0].child[k].pic}) no-repeat center"></i>
+				<span>${data[0].child[k].name}</span>
+			`)
+		zon_ul.append(lis)
+		}
+	}
+}
+//右侧内容渲染
+function contwen(data){
+	let list=null;
+	for (let k in data[0].child) {
+		if (k!='maxId') {
+			lis=document.createElement('li')
+			$(lis).data('path',data[0].child[k].path)
+			$(lis).addClass('wen')
+			$(lis).html(`
+				<div class="inct clearfix">
+					<span class="cecked"></span>
+					<div class="wencexct clearfix">
+						<em></em>
+						<span class="contwen"></span>
+						<span class="ctwms">${data[0].child[k].name}</span>
+						<input type="text" name="" class="wentext" value="${data[0].child[k].name}" />
+						<div class="conttshi clearfix">
+								<a href="javascript:;" class="w_share"></a>
+								<a href="javascript:;" class="w_down"></a>
+								<a href="javascript:;" class="w_del"></a>
+								<a href="javascript:;" class="w_remove"></a>
+								<a href="javascript:;" class="w_ren"></a>
+								<a href="javascript:;" class="w_safe"></a>							
+						</div>
+						<time>2016-12-22 10:41</time>
+					</div>							
+				</div>
+				<div class="delhied js_wendelhid">
+					<div class="mask"></div>
+					<div class="delcct js_wedecct">
+						<div class="delhed">
+							<h3>删除文件</h3>
+							<i class="delno"></i>
+						</div>
+						<div class="delcont">
+							<em></em>
+							<div class="tship">
+								<p>确定要删除这个文件夹吗？</p>
+								<p>已删除的文件可以在回收站找到</p>										
+							</div>
+							<div class="delbtn">
+								<a href="javascript:;" class="delok">确定</a>
+								<a href="javascript:;" class="delno">取消</a>									
+							</div>
+						</div>
+					</div>	
+				</div>				
+			`)
+			zhcoont.append(lis)		
+		}
+
+	}	
+}
+
+//右侧内容双击事件
+function dbclk(){
+	$('.wen').each((i,e)=>{
+		$(e).dblclick(()=>{
+			let path=$(e).data('path').split('_').join('/')
+			location.hash='h='+path
+		})
+	})
+}
+//改变hash值 找到点击的父级
+function getdata(data){
+	let hash=location.hash;
+	if (hash) {
+		let arr=hash.substr(1).split('=')[1].split('/')
+		let Data=data
+		for (let i=1,len=arr.length;i<len;i++) {
+			Data=Data[arr[i]]
+		}
+		return Data
+	}else{
+		return data
+	}
+}
+//右侧双击事件后再次渲染页面
+function newcont(Data,loddata){
+	//全部 的li标签
+	let loall=$('.loall')
+	//全部 内部的li标签
+	let lename=$('.lename')
+	if (Data.path.length>14) {
+		var xrdata=Data.path.substring(0,14)
+		
+	}else{
+		var xrdata=Data.path
+	}
+	let arr=xrdata.split('_')
+	//原始数组
+	let data3=data
+	for (let i=1,len=arr.length;i<len;i++) {
+			data3=data3[arr[i]]
+	}
+	//找到右侧匹配到的li 渲染小图标样式
+	lename.each((i,e)=>{
+		if ($(e).data('path')==data3.path) {
+			$(e).find('i').attr('style',`background:url(${data3.picnew}) no-repeat center`)}else if(loall.data('path')==data3.path){
+			loall.find('i').attr('style',`background:url(${loddata[0].pic}) no-repeat center`)
+			console.log(i)
+			$(e).find('i').attr('style',`background:url(${loddata[0].child[i].pic}) no-repeat center`)	
+		}else{
+//			if ($(e).data('path')==loddata[0].child[i].path) {
+				$(e).find('i').attr('style',`background:url(${loddata[0].child[i].pic}) no-repeat center`)
+			loall.find('i').attr('style',`background:url(${loddata[0].picnew}) no-repeat center`)				
+			}
+//		}
+	})
+/********************以上内容是渲染ul左侧侧边栏图标**********************/
+	$('.wen').remove()
+	let zhcoont=$('.zhcoont');
+	if (Data.child.maxId!=0) {
+		var obj=Data.child
+	let list=null;
+		for (let k in obj) {
+			if (k!='maxId') {
+				lis=document.createElement('li')
+				$(lis).data('path',obj[k].path)
+				$(lis).addClass('wen')
+				$(lis).html(`
+					<div class="inct clearfix">
+						<span class="cecked"></span>
+						<div class="wencexct clearfix">
+							<em></em>
+							<span class="contwen"></span>
+							<span class="ctwms">${obj[k].name}</span>
+							<input type="text" name="" class="wentext" value=">${obj[k].name}" />							
+							<div class="conttshi clearfix">
+								<a href="javascript:;" class="w_share"></a>
+								<a href="javascript:;" class="w_down"></a>
+								<a href="javascript:;" class="w_del"></a>
+								<a href="javascript:;" class="w_remove"></a>
+								<a href="javascript:;" class="w_ren"></a>
+								<a href="javascript:;" class="w_safe"></a>								
+							</div>
+							<time>2016-12-22 10:41</time>
+						</div>							
+					</div>
+					<div class="delhied js_wendelhid">
+						<div class="mask"></div>
+						<div class="delcct js_wedecct">
+							<div class="delhed">
+								<h3>删除文件</h3>
+								<i class="delno"></i>
+							</div>
+							<div class="delcont">
+								<em></em>
+								<div class="tship">
+									<p>确定要删除这个文件夹吗？</p>
+									<p>已删除的文件可以在回收站找到</p>										
+								</div>
+								<div class="delbtn">
+									<a href="javascript:;" class="delok">确定</a>
+									<a href="javascript:;" class="delno">取消</a>									
+								</div>
+							</div>
+						</div>	
+					</div>							
+				`)
+			}
+		zhcoont.append(lis)
+		}		
+	} else{
+		zhcoont.html('')
+	}
+	//重新设置n的值 关于全选的所有内容
+	n=0
+	//隐藏头部显示内容	
+	headhide()
+	//判断全选
+	pball()
+	//全选按钮触发
+	chall()
+}
+//左侧列表点击事件
+function lefclick(){
+	let lename=$('.lename')
+	lename.each((i,e)=>{
+		$(e).click(()=>{
+			let hash='h='+$(e).data('path').split('_').join('/')
+			location.hash=hash
+		})	
+	})
+}
+//<内容为全部> li的点击事件
+function lefallclick(){
+	//全部 的li标签
+	let loall=$('.loall')
+	loall.click(()=>{
+		let hash='h='+loall.data('path').split('_').join('/')
+		location.hash=hash
+	})
+}
+//生成面包屑导航
+function getdata2(data){
+	let hash=location.hash;
+	let arr1=[];
+	if (hash) {
+		let arr=hash.substr(1).split('=')[1].split('/')
+		let Data=data
+		/****************存在递归需要再过一遍**************/
+		fn();
+		function fn(){
+			if (arr.length>2) {
+				for (let i=1,len=arr.length;i<len;i++) {
+					Data=Data[arr[i]]
+				}
+				arr1.unshift(Data)
+				arr.splice(arr.length-2,2)
+				Data=data;
+				fn()
+			}else{
+				for (let i=1,len=arr.length;i<len;i++) {
+					Data=Data[arr[i]]
+				}
+				arr1.unshift(Data)
+			}
+		}
+		return arr1
+	}else{
+		return arr1=[]
+	}
+}
+//渲染面包屑导航
+function nav(arr){
+	let str='';
+	if (arr.length!=0) {
+		for (let i=0,len=arr.length;i<len;i++) {
+			str+=`<li class="navli">
+					<i></i>
+					<a href="javascript:;">${arr[i].name}</a>
+					</li>`
+	}
+		cotmbx.html(str)
+		$('.navli').each((i,e)=>{
+			$(e).data('path',arr[i].path)
+		})
+	}
+}
+//面包屑导航点击事件
+function rinavclick(){
+	let navli=$('.navli')
+	navli.each((i,e)=>{
+		$(e).click(()=>{
+			let hash='h='+$(e).data('path').split('_').join('/')		
+			location.hash=hash
+		})
+	})
+}
+//文件单一选中的事件
+function wenchecked(){
+	let wencek=$('.wen').find('.cecked')
+	wencek.click((ev)=>{
+		let evtar=$(ev.target);
+		if (evtar.data('onoff')) {
+			evtar.css({background:''})
+			evtar.data('onoff',false)
+			evtar.attr('onoff','0');
+			n--
+		}else{
+			evtar.css({background:'#3b93ff url(img/checkright.png) no-repeat center'})			
+			evtar.data('onoff',true);
+			evtar.attr('onoff','1');
+			n++
+		}
+		headhide()
+		
+		pball()
+		return false
+	})
+}
+//隐藏头部显示内容
+function headhide(){
+	if (n!=0) {		
+		hedhidobj.css({'display':'block'})
+		//选中个数统计
+		let xuceckd=$('.xuceckd')
+		xuceckd.text(`选择了${n}项`)	
+		//隐藏头部 取消选中按钮		
+		hidqx()
+		//隐藏头部 三个点按钮的鼠标移入移出事件
+		hiddian()
+		//隐藏头部 删除按钮触发
+		hedhiddel()
+		//隐藏头部 重命名事件
+		hidhcm()
+
+	} else{
+		hedhidobj.css({'display':''})			
+	}
+}
+//隐藏头部 取消选中按钮
+function hidqx(){
+	//取消选中按钮点击
+	let cancel=$('.cancel')
+	let wencek=$('.wen').find('.cecked')
+	cancel.click(()=>{
+		n=0	
+		wencek.css({background:''})
+		wencek.data('onoff',false)
+		wencek.attr('onoff','0');		
+		checkall.css({background:''})
+		checkall.data('onoff',false)
+		checkall.attr('onoff','0');		
+		hedhidobj.css({'display':''})
+	})
+}
+//隐藏头部 三个点按钮的鼠标移入移出事件
+function hiddian(){
+	let tumspan=$('.tumspan')
+	//隐藏的列表
+	let tuble=$('.tuble')
+	let hidlist=$('.tuble').find('ul')
+	let str=''
+	if (n>1) {
+		str=`<li class='hidydd'>
+				<a href="javascript:;">移动到</a>
+			</li>
+			<li class='hidbxx'>
+				<a href="javascript:;">移入保险箱</a>
+			</li>`
+	}else{
+		str=`
+			<li class='hidydd'>
+				<a href="javascript:;">移动到</a>
+			</li>
+			<li class='hidcmm'>
+				<a href="javascript:;" class="hidccm">重命名</a>
+			</li>
+			<li class='hidbxx'>
+				<a href="javascript:;">移入保险箱</a>
+			</li>
+		`
+	}
+	hidlist.html(str)
+	//	鼠标移入事件
+	tumspan.mouseenter(()=>{
+		tuble.fadeIn('fast')
+	})
+	//鼠标移出事件
+	tumspan.mouseleave(()=>{
+		tuble.fadeOut('fast')
+	})
+	tuble.mouseenter(()=>{
+		tuble.stop()
+	})
+	tuble.mouseleave(()=>{
+		tuble.fadeOut('fast')	
+	})	
+}
+//隐藏头部的删除按钮
+function hedhiddel(){
+	let js_haddel=$('.js_haddel')		
+	//删除弹窗
+	let js_hhiddelwen=hedhidobj.find('.js_hhiddelwen')
+	let js_wedecct=js_hhiddelwen.find('.js_wedecct')
+	js_haddel.click(()=>{
+				
+		//设置盒子水平居中的样式
+		let w=$(document).innerWidth()/2-js_wedecct.width()/2;
+		let h=$(document).innerHeight()/2-js_wedecct.height()/2;
+		js_wedecct.css({'top':h+'px','left':w+'px'})
+		let wenccc=$('.wen').find('.cecked[onoff=1]')
+		console.log(wenccc.length)
+		wenccc.each((i,e)=>{
+			js_hhiddelwen.css('display','block')
+			let path=$(e).closest('.wen').data('path')
+			let wen=$(e).closest('.wen')
+			//单一文件删除弹窗确认按钮事件
+			wentcdel(js_wedecct,js_hhiddelwen,path,wen)					
+		})
+	})
+	
+}
+//隐藏头部的重名事件
+function hidhcm(){
+	let hidccm=$('.hidccm')
+	console.log(hidccm)
+	hidccm.click(()=>{
+		let wenccc=$('.wen').find('.cecked[onoff=1]')
+		let wen=wenccc.closest('.wen')
+		let wentext=wen.find('.wentext')
+		let ctwms=wen.find('.ctwms');
+		wentext.css('display','block')
+		wentext.select()
+		wentext.blur((ev)=>{
+			ctwms.text($(ev.target).val())
+			wentext.css('display','none')
+			let path=wen.data('path')
+			let arr=path.split('_')
+			let Data=data
+			for (let i=1,len=arr.length;i<len;i++) {
+				Data=Data[arr[i]]
+			}
+			Data.name=$(ev.target).val()
+			//重绘左侧排版
+			list(data)
+			//左侧点击事件
+			lefclick()
+			//隐藏头部
+			hedhidobj.css({'display':''})			
+			//取消选中
+			wenccc.css({background:''})
+			wenccc.data('onoff',false)
+			wenccc.attr('onoff','0');
+			n--
+		})
+	})
+}
+//全选功能
+function chall(){
+	let wencek=$('.wen').find('.cecked')
+	checkall.click((ev)=>{
+		let tarev=$(ev.target)
+		if (tarev.data('onoff')){
+			wencek.css({background:''})
+			checkall.css({background:''})
+			tarev.data('onoff',false)
+			tarev.attr('onoff','0');
+			wencek.data('onoff',false)
+			wencek.attr('onoff','0');			
+			
+			n=0				
+		} else{				
+			checkall.css({background:'#3b93ff url(img/checkright.png) no-repeat center'})
+			wencek.css({background:'#3b93ff url(img/checkright.png) no-repeat center'})			
+			wencek.data('onoff',true)
+			wencek.attr('onoff','1');			
+			tarev.data('onoff',true);
+			tarev.attr('onoff','1');
+			n=wencek.length
+		}		
+		headhide()
+		return false
+	})
+}
+//单个文件选中，判断全选功能
+function pball(){
+	let wencek=$('.wen').find('.cecked')
+//	console.log(wencek.length,n)
+	if (n==wencek.length&&wencek.length!=0) {
+		checkall.css({background:'#3b93ff url(img/checkright.png) no-repeat center'})		
+		checkall.data('onoff',true)
+		checkall.attr('onoff','1')	
+		//选中个数统计
+		let xuceckd=$('.xuceckd')
+		xuceckd.text(`选择了${n}项`)	
+			
+	} else{
+		checkall.css({background:''})
+		checkall.data('onoff',false)
+		checkall.attr('onoff','0')	
+		if (n==wencek.length&&n==0) {
+			headhide()
+		}
+	}
+}
+//单一文件删除按钮
+function wendel(){
+	let w_del=$('.wen').find('.w_del')	
+//	console.log(w_del)
+	w_del.click((ev)=>{
+		let path=$(ev.target.closest('.wen')).data('path')
+		let wen=$(ev.target.closest('.wen'))
+		//找到删除文件弹窗
+		let js_wendelhid=$(ev.target.closest('.wen')).find('.js_wendelhid')
+		//删除弹窗内容
+		let js_wedecct=js_wendelhid.find('.js_wedecct')
+		//设置盒子水平居中的样式
+		let w=$(document).innerWidth()/2-js_wedecct.width()/2;
+		let h=$(document).innerHeight()/2-js_wedecct.height()/2;
+		js_wedecct.css({'top':h+'px','left':w+'px'})
+		//修改样式显示
+		js_wendelhid.css('display','block')		
+		//单一文件删除弹窗确认按钮事件
+		wentcdel(js_wedecct,js_wendelhid,path,wen)	
+	})
+}
+function wentcdel(js_wedecct,js_wendelhid,path,wen){
+	//参数1，传入的是弹窗内容
+	//参数2，传入弹窗 
+	//参数4，是删除的文件
+	/*
+	 1、拖拽功能
+	 2、点击删除功能
+	 3、取消弹窗功能
+	 * */
+	//删除文档弹窗的拖拽事件
+	js_wedecct.mousedown((ev)=>{
+		ev.preventDefault()
+		let t=ev.clientY-js_wedecct.offset().top;
+		let l=ev.clientX-js_wedecct.offset().left;
+		let maxt=$(document).innerHeight()-js_wedecct.height()
+		let maxl=$(document).innerWidth()-js_wedecct.width()
+		$(document).mousemove((ev)=>{
+			let newt=ev.clientY-t;
+			let newl=ev.clientX-l;
+			if (newt<0) {
+				newt=0
+			}
+			if (newl<0) {
+				newl=0
+			}
+			if (newt>maxt) {
+				newt=maxt
+			}
+			if (newl>maxl) {
+				newl=maxl
+			}
+			js_wedecct.css({
+				'top':newt+'px','left':newl+'px'
+			})
+		})
+		$(document).mouseup(()=>{
+			$(document).unbind('mousemove');
+		})
+	})
+	//取消选框功能触发
+	let delno=js_wedecct.find('.delno');
+	delno.click(()=>{
+		js_wendelhid.css('display','none')
+	})
+	//确认删除内容功能触发
+	let delok=js_wedecct.find('.delok');
+	delok.click((ev)=>{
+/******************删除按钮需要做的功能*************************************/
+		js_wendelhid.css('display','none')
+		//切割成数组
+		let arr=path.split('_')
+		let Data=data
+//		删除数据操作的原理:
+//			首先找到操作元素的父级;
+//			而数组中,抛去后两位,就为父级
+//			删除时注意,要删除父级下面的子级
+//			所以利用父级的child,在记录下子级的path值
+//			在将父级下的child的maxid也删除一位
+		let s=arr[arr.length-1]//子级的path的最后一位；
+		arr.splice(arr.length-2,2)
+		for (let i=1,len=arr.length;i<len;i++) {
+			Data=Data[arr[i]]
+		}
+		delete Data.child[s]
+		Data.child.maxId--
+		
+		wen.remove()
+		//右侧内容已经在上面执行中删除了DOM节点以及数据
+		let w_del=$('.wen').find('.w_del')
+		//为了判断全选用的
+		if (n==w_del.length+1) {
+			n--
+		}
+		//重绘左侧排版
+		list(data)
+		//左侧点击事件
+		lefclick()
+		//判断全选
+		pball()	
+		//总的全选框按钮的触发事件
+		chall()
+		hedhidobj.css({'display':''})
+	})
+}
+//重命名文件
+function rename(){
+	let w_ren=$('.wen').find('.w_ren')
+	w_ren.click((ev)=>{
+		let wen=$(ev.target).closest('.wen')
+		let wentext=wen.find('.wentext')
+		let ctwms=wen.find('.ctwms');
+		wentext.css('display','block')
+		wentext.select()
+		wentext.blur((ev)=>{
+			ctwms.text($(ev.target).val())
+			wentext.css('display','none')
+			let path=wen.data('path')
+			let arr=path.split('_')
+			let Data=data
+			for (let i=1,len=arr.length;i<len;i++) {
+				Data=Data[arr[i]]
+			}
+			Data.name=$(ev.target).val()
+			//重绘左侧排版
+			list(data)
+			//左侧点击事件
+			lefclick()			
+		})		
+	})
+}
+//全局调用框选
+boxSelect();
